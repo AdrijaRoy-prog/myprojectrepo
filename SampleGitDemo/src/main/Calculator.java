8d3fb7160a9369b6a76cb2f35df0213ec36d00d4
AdrijaRoy-prog <81158466+AdrijaRoy-prog@users.noreply.github.com>
2021-03-22 18:29:58 +0530

    add diff

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -9,5 +9,7 @@ add(10,20);
 	public static void add(int num1,int num2){
 	      System.out.println(num1+num2);
 	  }
+	public static void sub(int num1,int num2){
+	      System.out.println(num1-num2);
 
 }
