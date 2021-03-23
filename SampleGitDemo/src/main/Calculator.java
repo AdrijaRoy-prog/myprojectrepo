d69347cb5e2fe02007941de73ddc0610e943c16e
adrij <adrij@LAPTOP-B21TCTLO>
2021-03-23 09:51:13 +0530

    added square method in br1

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -15,4 +15,7 @@ add(10,20);
 	public static void prod(int num1,int num2){
 	      System.out.println(num1*num2);
 	}
+	public static void square(int num1,int num2){
+	      System.out.println(num1*num1);
+	}
 }
